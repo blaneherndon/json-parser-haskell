99c147b9f9d1ecd68b778cd52dd00184da874852
Blane Herndon <144256091+blaneherndon@users.noreply.github.com>
2025-12-24 17:41:46 -0600

    Finish parser

--- Main.hs
+++ Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
+import Control.Applicative
+import Data.Char
+
 data JsonValue
   = JsonNull
   | JsonBool Bool
-  | JsonNumber Double
-  | JsonString String
+  | JsonNumber Integer -- NOTE: This could be improved to support double
+  | JsonString String -- NOTE: This could be improved to support escapes
   | JsonArray [JsonValue]
   | JsonObject [(String, JsonValue)]
   deriving (Show, Eq)
@@ -58,6 +61,18 @@ instance Applicative Parser where
               Failure error
       )
 
+-- Make parser alternative
+-- NOTE: Picks first non empty
+instance Alternative Parser where
+  empty = Parser (\_ -> Failure "Alternative Property Error")
+  (Parser p1) <|> (Parser p2) =
+    Parser
+      ( \input ->
+          case p1 input of
+            Success x -> Success x
+            Failure _ -> p2 input
+      )
+
 -- Create parsers for each individual type first
 -- Create a helper parser to parse a single char
 parseChar :: Char -> Parser Char
@@ -78,22 +93,70 @@ jsonNull :: Parser JsonValue
 jsonNull = fmap (\_ -> JsonNull) (parseString "null")
 
 -- Create a parser that is capable to parse JsonBool
--- NOTE: Picks first non empty
 jsonBool :: Parser JsonValue
-jsonBool =
+jsonBool = fmap (\_ -> JsonBool True) (parseString "true") <|> fmap (\_ -> JsonBool False) (parseString "false")
+
+-- NOTE: We need to use span with a predicate
+parseSpan :: (Char -> Bool) -> Parser String
+parseSpan f =
   Parser
-    ( \input ->
-        case runParser (parseString "true") input of
-          Success (parsed, parsable) -> Success (JsonBool True, parsable)
-          Failure _ ->
-            case runParser (parseString "false") input of
-              Success (parsed, parsable) -> Success (JsonBool False, parsable)
-              Failure error -> Failure error
+    ( \input -> let (token, rest) = span f input in Success (token, rest)
     )
 
+-- Make a helper decorator
+notNull :: Parser [a] -> Parser [a]
+notNull (Parser p) =
+  Parser
+    ( \input -> case p input of
+        Success (parsed, parsable) ->
+          if null parsed
+            then Failure "notNull"
+            else Success (parsed, parsable)
+        Failure error -> Failure error
+    )
+
+-- Create a parser that is capable to parse JsonNumber
+jsonNumber :: Parser JsonValue
+jsonNumber = fmap (\digit -> JsonNumber (read digit)) (notNull (parseSpan isDigit))
+
+stringLiteral :: Parser String
+stringLiteral = (parseChar '"' *> parseSpan (/= '"') <* parseChar '"')
+
+-- Create a parser that is capable to parse JsonString
+jsonString :: Parser JsonValue
+jsonString = fmap JsonString stringLiteral
+
+-- Create a parser that is capable to parse JsonArray
+ws :: Parser String
+ws = parseSpan isSpace
+
+seperateBy :: Parser a -> Parser b -> Parser [b]
+seperateBy delim element = fmap (:) element <*> many (delim *> element) <|> pure []
+
+-- Example: runParser (many jsonNull) "nullnullnull" => Success ([JsonNull,JsonNull,JsonNull],"")
+jsonArray :: Parser JsonValue
+jsonArray = fmap JsonArray (parseChar '[' *> ws *> elements <* ws <* parseChar ']')
+  where
+    elements = seperateBy (ws *> parseChar ',' <* ws) jsonValue
+
+-- Create a parser that is capable to parse JsonObject
+jsonObject :: Parser JsonValue
+jsonObject =
+  fmap
+    JsonObject
+    ( parseChar '{'
+        *> ws
+        *> seperateBy (ws *> parseChar ',' <* ws) pair
+        <* ws
+        <* parseChar '}'
+    )
+  where
+    pair =
+      fmap (\key _ value -> (key, value)) stringLiteral <*> (ws *> parseChar ':' <* ws) <*> jsonValue
+
 -- Create a parser that is capable to parse JsonValue
-jsonValue :: parser JsonValue
-jsonValue = undefined
+jsonValue :: Parser JsonValue
+jsonValue = jsonNull <|> jsonBool <|> jsonNumber <|> jsonString <|> jsonArray <|> jsonObject
 
 main :: IO ()
 main = undefined
